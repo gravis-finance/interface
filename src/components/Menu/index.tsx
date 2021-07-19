dfc68b6153eef36fcaf3df2e5035781a5debb87e
Андрей <justjectis@gmail.com>
2021-07-19 14:51:42 +0300

    Added menu links

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -96,6 +96,29 @@ const Menu: React.FC<{ loginBlockVisible?: boolean }> = ({ loginBlockVisible, ..
         ],
       },
       {
+        label: t('mainMenu.nftmarket'),
+        icon: 'NFTMarketIcon',
+        items: [
+          {
+            label: t('buyNFT'),
+            href: `${process.env.REACT_APP_GMART_URL}/buy`,
+          },
+          {
+            label: t('sellNFT'),
+            href: `${process.env.REACT_APP_GMART_URL}/sell`,
+          },
+          {
+            label: t('sendNFT'),
+            href: `${process.env.REACT_APP_GMART_URL}/transfer`,
+          },
+        ]
+      },
+      {
+        label: t('mainMenu.NFTFarming'),
+        icon: 'NFTFarmingIcon',
+        href: process.env.REACT_APP_NFTFARMING_URL
+      },
+      {
         label: t('mainMenu.more'),
         icon: 'MoreIcon',
         items: [
